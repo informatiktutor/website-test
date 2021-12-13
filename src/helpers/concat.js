e57343bab0ae2d6b52833d90591cd98061f50abb
Jonas van den Berg <jonas@informatiktutor.de>
2021-12-13 17:52:47 +0100

    Create footer

--- src/helpers/concat.js
+++ src/helpers/concat.js
@@ -1,6 +1,12 @@
 module.exports = function (a1, a2) {
   if (Array.isArray(a1) && Array.isArray(a2)) {
-    return a1.concat(a2);
+    return a1.concat(a2)
   }
-  return a1 + a2;
-};
+  if (!a1) {
+    return a2
+  }
+  if (!a2) {
+    return a1
+  }
+  return a1 + a2
+}
